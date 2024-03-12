c474007394247104505a723f80a43c9f1d40e509
Neegmo <miljkovicpavle1@gmail.com>
2024-03-12 14:17:53 +0100

    updated the game

--- src/HelloWorldScene.js
+++ src/HelloWorldScene.js
@@ -1,134 +1,104 @@
 import Phaser from "phaser";
+import Glass from "./classes/Glass";
 
 export default class HelloWorldScene extends Phaser.Scene {
   constructor() {
     super("hello-world");
   }
 
-  glass1;
-  glass1Liquid;
-  glass1Color;
-  glass2;
-  glass2Liquid;
-  glas2Color;
-  glass3;
-  glass3Liquid;
-  glass3Color;
-
   nextShot;
 
   getNextColorButton;
 
-  colorSequence = ["Red", "Green", "Blue", "Yellow", "Pink", "Cyan"];
+  colorSequence = ["Pink", "Purple", "Green", "Yellow", "Orange"];
 
   nextColor;
+
+  bet = 10;
+
+  canGenerateColor = true;
 
   preload() {
     this.load.baseURL = "Assets/";
 
     this.load.image("BG", "BG.png");
     this.load.image("Glass", "Glass.png");
-    this.load.image("Red", "Red.png");
+    this.load.image("ShotGlass", "ShotGlass.png");
+    this.load.image("Orange", "Orange.png");
     this.load.image("Green", "Green.png");
     this.load.image("Blue", "Blue.png");
     this.load.image("Black", "Black.png");
-	this.load.image("Yellow", "Yellow.png");
-	this.load.image("Pink", "Pink.png");
-	this.load.image("Cyan", "Cyan.png");
+    this.load.image("Yellow", "Yellow.png");
+    this.load.image("Pink", "Pink.png");
+    this.load.image("Purple", "Purple.png");
   }
 
   create() {
-    this.add.image(0, 0, "BG").setScale(2, 2);
+    this.add.image(0, 0, "BG").setOrigin(0, 0);
 
-    this.createFirstGlass(230, 800);
-    this.createSecondGlass(530, 800);
-    this.createThirdGlass(830, 800);
+    this.createGlasses();
 
     this.createGetNextColorButton();
 
-	console.log(this.glass1Liquid.scaleY)
+    this.createHUD();
+
+    this.shotGlass = this.add.image(530, 1500, "ShotGlass").setScale(0.5, 0.5);
   }
 
-  createFirstGlass(x, y) {
-    this.glass1 = this.add.image(x, y, "Glass");
-    this.glass1.setDepth(5);
-    this.glass1Liquid = this.add.image(x - 100, y + 200, "Black");
-    this.glass1Liquid.setOrigin(0, 1);
-    this.glass1Liquid.setAlpha(0);
-    this.glass1Liquid.scaleY = 0.8;
-    this.glass1.setInteractive();
-    this.glass1.on("pointerover", () => {
-      this.glass1.setAlpha(0.5);
-    });
-    this.glass1.on("pointerout", () => {
-      this.glass1.setAlpha(1);
-    });
-    this.glass1.on("pointerdown", () => {
-      this.fillGlass(this.glass1Liquid);
-    });
-  }
-
-  createSecondGlass(x, y) {
-    this.glass2 = this.add.image(x, y, "Glass");
-    this.glass2.setDepth(5);
-    this.glass2Liquid = this.add.image(x - 100, y + 200, "Black");
-    this.glass2Liquid.setOrigin(0, 1);
-    this.glass2Liquid.setAlpha(0);
-    this.glass2Liquid.scaleY = 0.8;
-    this.glass2.setInteractive();
-    this.glass2.on("pointerover", () => {
-      this.glass2.setAlpha(0.5);
-    });
-    this.glass2.on("pointerout", () => {
-      this.glass2.setAlpha(1);
-    });
-    this.glass2.on("pointerdown", () => {
-      this.fillGlass(this.glass2Liquid);
-    });
-  }
-
-  createThirdGlass(x, y) {
-    this.glass3 = this.add.image(x, y, "Glass");
-    this.glass3.setDepth(5);
-    this.glass3Liquid = this.add.image(x - 100, y + 200, "Black");
-    this.glass3Liquid.setOrigin(0, 1);
-    this.glass3Liquid.setAlpha(0);
-    this.glass3Liquid.scaleY = 0.8;
-    this.glass3.setInteractive();
-    this.glass3.on("pointerover", () => {
-      this.glass3.setAlpha(0.5);
-    });
-    this.glass3.on("pointerout", () => {
-      this.glass3.setAlpha(1);
-    });
-    this.glass3.on("pointerdown", () => {
-      this.fillGlass(this.glass3Liquid);
-    });
-  }
-
-  fillGlass(liquid) {
+  update() {
     if (this.nextShot === undefined) return;
+    this.nextShot.x = this.shotGlass.x
+    this.nextShot.y = this.shotGlass.y + 25
+    // this.nextShot.Rotation = this.shotGlass.rotation
+    this.nextShot.setRotation(this.shotGlass.rotation)
+  }
 
-    console.log(this.glass1Liquid);
-    if (liquid.texture.key === this.nextColor || liquid.scaleY === 0.8) {
-      liquid.setTexture(this.nextColor);
-    } else {
-      liquid.setTexture("Black");
-    }
-    liquid.setAlpha(1);
-    liquid.scaleY += 0.2;
-    this.nextShot.destroy();
-    this.nextShot = undefined;
+  createHUD() {
+    this.balance = 1000;
+    this.balanceText = this.add.text(20, 100, `Balance: ${this.balance}`, {
+      fontSize: "40px",
+    });
+  }
+
+  createGlasses() {
+    this.glass1 = new Glass(this, 280, 1100, "Glass");
+    this.glass1.setScale(0.5, 0.5);
+    this.add.existing(this.glass1);
+
+    this.glass2 = new Glass(this, 530, 1100, "Glass");
+    this.glass2.setScale(0.5, 0.5);
+    this.add.existing(this.glass2);
+
+    this.glass3 = new Glass(this, 780, 1100, "Glass");
+    this.glass3.setScale(0.5, 0.5);
+    this.add.existing(this.glass3);
   }
 
   getRandomColor() {
-    this.nextColor = this.colorSequence[Phaser.Math.Between(0, 5)];
+    this.nextColorNumber = Phaser.Math.Between(1, 100);
+    console.log(this.nextColorNumber)
+
+    if (this.nextColorNumber < 6) {
+      this.nextColor = this.colorSequence[0];
+    } else if (this.nextColorNumber < 18) {
+      this.nextColor = this.colorSequence[1];
+    } else if (this.nextColorNumber < 30) {
+      this.nextColor = this.colorSequence[2];
+    } else if (this.nextColorNumber < 66) {
+      this.nextColor = this.colorSequence[3];
+    } else {
+      this.nextColor = this.colorSequence[4];
+    }
   }
 
   createNextShot() {
-    if (this.nextShot != undefined) return;
+    if (this.nextShot != undefined || !this.canGenerateColor) return;
     this.getRandomColor();
-    this.nextShot = this.add.image(540, 1500, this.nextColor);
+    this.nextShot = this.add
+      .image(530, 1525, this.nextColor)
+      .setScale(0.5, 0.5);
+    this.updateBalance(-10)
+    this.canGenerateColor  = false;
   }
 
   createGetNextColorButton() {
@@ -144,5 +114,53 @@ export default class HelloWorldScene extends Phaser.Scene {
     this.getNextColorButton.on("pointerdown", () => {
       this.createNextShot();
     });
+  }
+
+  moveShotGlassToTarget(glass) {
+    let x = this.shotGlass.x;
+    let y = this.shotGlass.y;
+
+    this.tweens.add({
+      targets: this.shotGlass,
+      x: glass.x + 50,
+      y: glass.y - 400,
+      yoyo: false,
+      repeat: 0,
+      ease: "Sine.easeOut",
+      duration: 500,
+      onComplete: () => {
+        console.log("Movement completed");
+        glass.addNextShot(this.nextShot.texture)
+        this.nextShot.destroy()
+        this.nextShot = undefined
+        this.tweens.add({
+          targets: this.shotGlass,
+          angle: "-=120",
+          yoyo: true,
+          ease: "Sine.easeInOut",
+          duration: 500,
+          onComplete: () => {
+            console.log("Rotation complete!");
+            this.tweens.add({
+              targets: this.shotGlass,
+              x: x,
+              y: y,
+              yoyo: false,
+              repeat: 0,
+              ease: "Sine.easeOut",
+              duration: 500,
+              onComplete: () => {
+                this.canGenerateColor = true
+              }
+            });
+          },
+        });
+      },
+    });
+  }
+
+  updateBalance(ammount) {
+    this.balance += ammount
+    this.balanceText.text = `Balance: ${this.balance}`
   }
 }
---
+++ src/classes/Glass.js
@@ -0,0 +1,108 @@
+export default class Glass extends Phaser.GameObjects.Sprite {
+  constructor(scene, x, y, texture) {
+    super(scene, x, y, texture);
+
+    this.scene = scene;
+    this.sellButton = undefined;
+    this.colorBetIndex = 0;
+    this.colorSequence = [];
+    this.colorHight = 0;
+    this.price = 0;
+
+    this.highestPriceSequence = [2, 8.5, 160, 2500, 10000, 50000];
+    this.middlePriceSequence = [0.5, 1.5, 13, 125, 1000, 5000];
+    this.smallestPriceSequence = [0.25, 0.75, 2, 5.5, 15, 40];
+
+    this.setInteractive();
+
+    this.on("pointerup", () => {
+      if (this.colorSequence.length >= 6) return;
+      scene.moveShotGlassToTarget(this);
+    });
+
+    this.sellGlass = scene.add
+      .text(this.x, this.y + 220, `SELL FOR:\n${this.price}`, {
+        font: "600 30px Roboto",
+        align: "center",
+      })
+      .setOrigin(0.5, 0.5);
+
+    this.sellGlass.setInteractive();
+
+    this.sellGlass.on("pointerup", () => {
+      if (!scene.canGenerateColor) return;
+      this.colorSequence.forEach((element) => {
+        element.destroy()
+      });
+      this.colorSequence = []
+      this.colorHight = 0
+      this.scene.updateBalance(this.price)
+
+      this.price = 0;
+      this.sellGlass.text = `SELL FOR:\n${this.price}`;
+
+    });
+  }
+
+  addNextShot(color) {
+    // this.colorSequence = [...this.colorSequence, color]
+
+    let shotColor = this.scene.add
+      .sprite(this.x, this.y + 150 + this.colorHight, color)
+      .setScale(0.5, 0.5);
+    this.colorHight -= 50;
+    if (this.colorSequence[0] === undefined) {
+      this.colorSequence.push(shotColor);
+    } else if (shotColor.texture === this.colorSequence[0].texture) {
+      this.colorSequence.push(shotColor);
+    } else {
+      this.colorSequence.push(shotColor);
+      this.colorSequence.forEach((element) => {
+        element.setTexture("Black");
+      });
+      this.scene.time.delayedCall(500, () => {
+        this.colorSequence.forEach((element) => {
+          element.destroy()
+        });
+        this.colorSequence = []
+        this.colorHight = 0
+        this.price = 0
+  
+        this.price = 0;
+        this.sellGlass.text = `SELL FOR:\n${this.price}`;
+      })
+    }
+    console.log(this.colorSequence[0].texture.key);
+    switch (this.colorSequence[0].texture.key) {
+      case "Pink":
+        this.price =
+          this.scene.bet *
+          this.highestPriceSequence[this.colorSequence.length - 1];
+        break;
+      case "Purple":
+        this.price =
+          this.scene.bet *
+          this.middlePriceSequence[this.colorSequence.length - 1];
+        break;
+      case "Green":
+        this.price =
+          this.scene.bet *
+          this.middlePriceSequence[this.colorSequence.length - 1];
+        break;
+      case "Orange":
+        this.price =
+          this.scene.bet *
+          this.smallestPriceSequence[this.colorSequence.length - 1];
+        break;
+      case "Yellow":
+        this.price =
+          this.scene.bet *
+          this.smallestPriceSequence[this.colorSequence.length - 1];
+        break;
+      case "Black":
+        this.price = 0;
+        break;
+    }
+    this.sellGlass.text = `SELL FOR:\n${this.price}`;
+  }
+}
