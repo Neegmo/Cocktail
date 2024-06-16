e8db274f967f68012a64b197c6fe21ad9488b92d
Neegmo <miljkovicpavle1@gmail.com>
2024-06-16 12:21:14 +0200

    Fixed background

--- src/main.js
+++ src/main.js
@@ -1,23 +1,23 @@
-import Phaser from 'phaser'
+import Phaser from "phaser";
 
-import HelloWorldScene from './HelloWorldScene'
+import HelloWorldScene from "./HelloWorldScene";
 
 const config = {
-	type: Phaser.AUTO,
-	parent: 'app',
-	scale: {
-        mode: Phaser.Scale.FIT,
-		autoCenter: Phaser.Scale.CENTER_HORIZONTALLY,
-        width: 1071,
-        height: 2120
+  type: Phaser.AUTO,
+  parent: "app",
+  scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_HORIZONTALLY,
+    width: 1071,
+    height: 2100,
+  },
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { y: 0 },
     },
-	physics: {
-		default: 'arcade',
-		arcade: {
-			gravity: { y: 0 },
-		},
-	},
-	scene: [HelloWorldScene],
-}
+  },
+  scene: [HelloWorldScene],
+};
 
-export default new Phaser.Game(config)
+export default new Phaser.Game(config);
